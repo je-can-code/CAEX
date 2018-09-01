68dc56f32c7c74205ea56a3fefe87beb9acb9b9b
Jeremy <jragyn@gmail.com>
2018-09-01 11:09:51 -0700

    menu does things
    doesn't allocate, but does draw and update per index of items.

--- _project/js/plugins/J_SDP.js
+++ _project/js/plugins/J_SDP.js
@@ -27,7 +27,7 @@ Imported["JE Stat Distribution Panels"] = "0.1.0";
 
 // Creates and returns a "panel" for use to upgrade with points.
 // This panel is pushed into a given player's panel list.
-J.SD.MakePanel = (na = "default", sy = "def", cat = 0, fp = "flat",
+J.SD.MakePanel = (na = "default name", sy = "def", cat = 0, fp = "flat",
                   ea = 5, max = 10, cur = 0, cmult = 1.2, cgrow = 50) => {
   let panel = {
     name: na,           // the display name of the panel
@@ -48,8 +48,25 @@ J.SD.MakePanel = (na = "default", sy = "def", cat = 0, fp = "flat",
 J.SD.GetCommandName = function() {
   //var p = PluginManager.parameters('J_SDP');
   //return p['cmdName'];
-  let name = "Distribution";
+  let name = "P A N E L S";
   return name;
+};
+
+J.SD.TranslateParam = function(p) {
+  switch (p) {
+    case 0: case 1: case 2: case 3: case 4: case 5:
+    case 6: case 7: return TextManager.param(p);
+
+    case 8: case 9: case 10: case 11: case 12:
+    case 13: case 14: case 15: case 16:
+    case 17: return TextManager.sparam(p-8);
+
+    case 18: case 19: case 20: case 21: case 22:
+    case 23: case 24: case 25: case 26:
+    case 27: return TextManager.xparam(p-18);
+    default: return "invalid parameter"
+  }
+
 };
 
 J.SD.visibility = true;
@@ -70,9 +87,12 @@ J.SD.visibility = true;
   Game_Actor.prototype.initSDP = function() {
     this._sdpCollection = [];   // the collection of panels will be in here.
     this._sdpPts = 0;           // how many points you have to be distributed across your panels.
-    this.SDP_addPanel(_.MakePanel());
-    this.SDP_addPanel(_.MakePanel("some other", "sec"));
-    this.SDP_addPanel(_.MakePanel("yet another", "thr"));
+    
+    // sample panels
+    this.SDP_addPanel(_.MakePanel()); // default panel
+    this.SDP_addPanel(_.MakePanel("Power I", "pwr01",  2, "flat", 7, 20, 0, 1.3, 20));
+    this.SDP_addPanel(_.MakePanel("EXP+ IV", "cri04", 17, "perc", 5, 10, 0, 3.6, 50));
+    this.SDP_addPanel(_.MakePanel("HRG+ II", "cri04", 25, "flat", 1,  5, 0, 2.1, 30));
   };
   
   // add/subtract distribution points.
@@ -135,11 +155,26 @@ J.SD.visibility = true;
     Scene_MenuBase.prototype.initialize.call(this);
   };
 
-  // core creation function of the Difficulty scene.
+  // core creation function of the Stat Distribution Panel scene.
   Scene_SDP.prototype.create = function() {
     Scene_MenuBase.prototype.create.call(this);
+    this.createHelpWindow();      // the description of the panel.
     this.createCommandWindow();   // the list of distribution panels.
-    //this.createHelpWindow();      // the description of the panel.
+    this._index = this._sdpWindow.index();
+  };
+
+  // assists in the panel tracking.
+  Scene_SDP.prototype.update = function() {
+    Scene_MenuBase.prototype.update.call(this);
+    if (this._index != this._sdpWindow.index())
+      this.updateIndex();
+  };
+
+  // tracks the index for keeping panel up to date.
+  Scene_SDP.prototype.updateIndex = function() {
+    this._index = this._sdpWindow.index();
+    this._sdpDetails.changePanel(this._index);
+    console.log(this._index);
   };
 
   // the list of distribution panels based on the character.
@@ -148,7 +183,6 @@ J.SD.visibility = true;
     this._sdpWindow.setHandler('ok',      this.onSDPok.bind(this));
     this._sdpWindow.setHandler('cancel',  this.popScene.bind(this));
     this.addWindow(this._sdpWindow);
-
   };
 
   Scene_SDP.prototype.onSDPok = function() {
@@ -156,7 +190,9 @@ J.SD.visibility = true;
   }
 
   Scene_SDP.prototype.createHelpWindow = function() {
-    // draw the description of the panel selected, here.
+    this._sdpDetails = new Window_SDP_Details();
+    this._sdpDetails.refresh();
+    this.addWindow(this._sdpDetails);
   };
 
   /* -------------------------------------------------------------------------- */
@@ -164,11 +200,9 @@ J.SD.visibility = true;
 
   function Window_SDP_List() { this.initialize.apply(this, arguments); }
   Window_SDP_List.prototype = Object.create(Window_Command.prototype);
-  Window_SDP_List.prototype.constructor = Window_Command;
+  Window_SDP_List.prototype.constructor = Window_MenuCommand;
 
   Window_SDP_List.prototype.initialize = function() {
-    let width = Graphics.boxWidth;
-    let height = Graphics.boxHeight;
     let x = 0;
     let y = 0;
     this._list = [];
@@ -177,7 +211,15 @@ J.SD.visibility = true;
     this._actor = null;
     this.setActor($gameParty.members()[0]);
     this.refresh();
-    Window_Command.prototype.initialize.call(this, x, y, width, height);
+    Window_Command.prototype.initialize.call(this, x, y);
+  };
+
+  Window_SDP_List.prototype.windowWidth = function() {
+    return Graphics.boxWidth;
+  };
+
+  Window_SDP_List.prototype.numVisibleRows = function() {
+    return 6;
   };
 
   Window_SDP_List.prototype.setActor = function(actor) {
@@ -242,6 +284,75 @@ J.SD.visibility = true;
     // _listMod = _list
     // reduce points
     // empty temp variables
+  };
+
+  /* -------------------------------------------------------------------------- */
+  function Window_SDP_Details() {this.initialize.apply(this, arguments); }
+
+  Window_SDP_Details.prototype = Object.create(Window_Base.prototype);
+  Window_SDP_Details.prototype.constructor = Window_Base;
+  
+  Window_SDP_Details.prototype.initialize = function() {
+    var width = Graphics.boxWidth;
+    var height = Graphics.boxHeight;
+    var x = 0;
+    var y = 350;
+    this._actor = null;
+    this._currentPanel = 0;
+    this.setActor($gameParty.members()[0]);
+    Window_Base.prototype.initialize.call(this, x, y, width, height);
+    this.refresh();
+  };
+
+  Window_SDP_Details.prototype.setActor = function(actor) {
+    if (this._actor === null) this._actor = $gameParty.members()[0];
+    if (this._actor !== actor) {
+        this._actor = actor;
+        this.refresh();
+    }
+  };
+
+  Window_SDP_Details.prototype.refresh = function() {
+    this.contents.clear();
+    this.drawSDPDetails(this._currentPanel);
+  };
+
+  Window_SDP_Details.prototype.changePanel = function(nextPanel) {
+    if (this._currentPanel !== nextPanel) {
+      this._currentPanel = nextPanel;
+      this.refresh();
+    }
+  };
+/*
+  name: na,           // the display name of the panel
+  sym: sy,            // the unique symbol of the panel for listing
+  category: cat,      // some parameter
+  flatOrPercent: fp,  // flat/percent increase/decrease decision.
+  perRank: ea,        // how much the increase/decrease is.
+  rankMax: max,       // some number that defines how high it can go; 0 if infinite.
+  rankCur: cur,       // always 0; increments +1 per rank up.
+  cost: () => { // the cost formula, based on rankCur.
+    if (rankCur === rankMax) return 0;
+    return Math.floor(cmult * (cgrow * (this.rankCur + 1)));
+  },
+*/
+
+  Window_SDP_Details.prototype.drawSDPDetails = function() {
+    let lh = this.lineHeight();
+    let panel = this._actor._sdpCollection[this._currentPanel];
+    let forp = panel.flatOrPercent == "flat" ? "" : "%";
+    //let stat = this._actor.param(panel.category)
+    
+    this.drawText(panel.name, 0, lh*0, 255);
+    // draw description lh*1 ???
+
+    this.drawText(_.TranslateParam(panel.category), 0, lh*2, 255);
+    
+    this.drawText(panel.rankCur, 0, lh*3, 255);
+    this.drawText("/", 40, lh*3, 255);
+    this.drawText(panel.rankMax, 80, lh*3, 255);
+
+    this.drawText("+" + panel.perRank + forp + " per level", 160, lh*2, 255);
   };
   
 
