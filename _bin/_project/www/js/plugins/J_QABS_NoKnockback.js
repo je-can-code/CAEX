b90eb4c199b9472cd2144f5b60e96c37e012f90b
JE <jragyn@outlook.com>
2018-09-30 06:20:46 -0700

    removed builds from repo.

--- _bin/_project/www/js/plugins/J_QABS_NoKnockback.js
+++
@@ -1,8 +0,0 @@
-
-Game_Enemy.prototype.isHeavy = function(id) {
-  var enemy = $dataEnemies[id];
-  var nd = enemy.note.split(/[\r\n]+/);
-  var rgx = /<heavy>/i;
-  for (var n = 0; n < nd.length; n++) { if (nd[n].match(rgx)) { return true; } }
-  return false;
-};
--- _exe/_project/www/js/plugins/========================.js
+++
@@ -1,1 +0,0 @@
-// spacer for plugin manager
